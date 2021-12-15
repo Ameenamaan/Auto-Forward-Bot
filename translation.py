331ed9952a2b71dbecfcfeebd4a2246acf946cc4
Ameenamaan <88217467+Ameenamaan@users.noreply.github.com>
2021-12-15 14:01:41 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -22,7 +22,7 @@ More details /help</i>"""
   ABOUT_TXT = """<b><u>My Info</b></u>
 
 <b>Name :</b> <code>Auto Forward Bot</code>
-<b>Credit :</b> <code>Dark Angel</code>
+<b>Credit :</b> <code>⸙ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋᵐʳ𝐔𝐧𝐤𝐧𝐨𝐰𝐧🇮🇳༒</code>
 <b>Language :</b> <code>Python3</code>
 <b>Lybrary :</b> <code>Pyrogram v1.2.9</code>
 <b>Server :</b> <code>Heroku</code>
