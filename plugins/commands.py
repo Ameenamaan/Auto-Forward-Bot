8f391831002b016c7f7739d7d514bcd41a4860c9
Ameenamaan <88217467+Ameenamaan@users.noreply.github.com>
2021-12-15 14:05:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,11 +15,11 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMedi
 @Client.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
     buttons = [[
-        InlineKeyboardButton('📜 Support Group', url='https://t.me/DxHelpDesk'),
-        InlineKeyboardButton('Update Channel ♻️', url='https://t.me/DX_Botz')
+        InlineKeyboardButton('♻️ Movie Group ♻️', url='https://t.me/DxHelpDesk'),
+        InlineKeyboardButton('♻️ Files Channel ♻️', url='https://t.me/DX_Botz')
         ],[
-        InlineKeyboardButton('💡 SouceCode', url='https://github.com/Jijinr/Frwdit-V2'),
-        InlineKeyboardButton('String Session 🎻', url ='https://replit.com/@JijinR/PyroSessionString?v=1')
+        InlineKeyboardButton('💡 SouceCode', url='https://github.com/Ameenamaan/Auto-Forward-Bot'),
+        InlineKeyboardButton('Dev 🤴', url ='https://t.me/Ameen_Ameen_12')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
@@ -34,7 +34,7 @@ async def start(client, message):
 @Client.on_message(filters.private & filters.command(['help']))
 async def help(client, message):
     buttons = [[
-        InlineKeyboardButton('SouceCode 💡', url='https://github.com/Jijinr/Frwdit-V2'),
+        InlineKeyboardButton('SouceCode 💡', url='https://github.com/Ameenamaan/Auto-Forward-Bot'),
         InlineKeyboardButton('close 🔐', callback_data='close_btn')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
@@ -49,7 +49,7 @@ async def help(client, message):
 @Client.on_message(filters.private & filters.command(['about']))
 async def about(client, message):
     buttons = [[
-        InlineKeyboardButton('💡 SouceCode', url='https://github.com/Jijinr/Frwdit-V2'),
+        InlineKeyboardButton('💡 SouceCode', url='https://github.com/Ameenamaan/Auto-Forward-Bot'),
         InlineKeyboardButton('close 🔐', callback_data='close_btn')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
